13a3206d2f665abef833eefa54fe2f70d3625a34
Eduardus Bernard Jayadinata <z5486996@ad.unsw.edu.au>
2026-07-04 22:17:52 +1000

    Added request for mic perms

--- src/lib/useClassifier.ts
+++ src/lib/useClassifier.ts
@@ -2,15 +2,30 @@ import * as base64js from 'base64-js';
 import { useCallback, useRef } from 'react';
 import LiveAudioStream from 'react-native-live-audio-stream';
 import { useModel } from './useModel';
+import { PermissionsAndroid, Platform } from "react-native";
 
 const WINDOW_SIZE = 15600;
+
+async function requestMicPermission() {
+  if (Platform.OS !== "android") return true;
+
+  const res = await PermissionsAndroid.request(
+    PermissionsAndroid.PERMISSIONS.RECORD_AUDIO
+  );
+
+  return res === PermissionsAndroid.RESULTS.GRANTED;
+}
 
 export function useClassifier(onResult: (label: string, score: number) => void) {
   const { ready, classify } = useModel();
   const bufferRef = useRef<number[]>([]);
 
-  const start = useCallback(() => {
+  const start = useCallback(async() => {
     if (!ready) return;
+
+    const granted = await requestMicPermission();
+    if (!granted) return;
+
     bufferRef.current = [];
 
     LiveAudioStream.init({
